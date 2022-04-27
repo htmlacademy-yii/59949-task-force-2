27004903b711dee0aa6b8d2c69bea374cdd2a534
Sergey <isa86box@gmail.com>
2022-04-27 21:59:27 +0300

    внес правки в схему БД по замечаниям наставника https://github.com/htmlacademy-yii/59949-task-force-2/pull/4

--- src/schema.sql
+++ src/schema.sql
@@ -4,108 +4,113 @@ CREATE DATABASE taskforce
 
 USE taskforce;
 
-CREATE TABLE categories (
+CREATE TABLE categories
+(
     PRIMARY KEY (id),
-    id              INT              NOT NULL        auto_increment,
-    title           VARCHAR(255)     NOT NULL,
-    code            VARCHAR(255)     NOT NULL,
-    created_at      DATETIME         NOT NULL        DEFAULT NOW(),
-    updated_at      DATETIME         NOT NULL,
-    deleted_at      DATETIME         NOT NULL
+    id         INT UNSIGNED NOT NULL auto_increment,
+    title      VARCHAR(255) NOT NULL,
+    code       VARCHAR(100) NOT NULL,
+    created_at DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at DATETIME     NOT NULL,
+    deleted_at DATETIME     NOT NULL
 );
 
-CREATE TABLE statuses (
+CREATE TABLE files
+(
     PRIMARY KEY (id),
-    id              INT              NOT NULL        auto_increment,
-    title           VARCHAR(255)     NOT NULL,
-    code            VARCHAR(255)     NOT NULL,
-    created_at      DATETIME         NOT NULL        DEFAULT NOW(),
-    updated_at      DATETIME         NOT NULL,
-    deleted_at      DATETIME         NOT NULL
+    id         INT UNSIGNED NOT NULL auto_increment,
+    path       VARCHAR(255) NOT NULL,
+    created_at DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at DATETIME     NOT NULL,
+    deleted_at DATETIME     NOT NULL
 );
 
-CREATE TABLE cities (
+CREATE TABLE cities
+(
     PRIMARY KEY (id),
-    id              INT              NOT NULL        auto_increment,
-    name            VARCHAR(255)     NOT NULL,
-    code            VARCHAR(255)     NOT NULL,
-    created_at      DATETIME         NOT NULL        DEFAULT NOW(),
-    updated_at      DATETIME         NOT NULL,
-    deleted_at      DATETIME         NOT NULL
+    id          INT UNSIGNED NOT NULL auto_increment,
+    name        VARCHAR(100) NOT NULL,
+    coordinates POINT,
+    created_at  DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at  DATETIME     NOT NULL,
+    deleted_at  DATETIME     NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE users
+(
     PRIMARY KEY (id),
-    id              INT              NOT NULL        auto_increment,
-    type            VARCHAR(255)     NOT NULL,
-    created_at      DATETIME         NOT NULL        DEFAULT NOW(),
-    updated_at      DATETIME         NOT NULL,
-    deleted_at      DATETIME         NOT NULL
+    id                 INT UNSIGNED NOT NULL auto_increment,
+    email              VARCHAR(128) NOT NULL UNIQUE,
+    name               VARCHAR(150) NOT NULL,
+    password           CHAR(64)     NOT NULL,
+    phone              VARCHAR(20)  NOT NULL,
+    telegram           VARCHAR(100) NOT NULL,
+    description        VARCHAR(500) NOT NULL,
+    birth_date         DATETIME     NULL,
+    avatar_file_id     INT UNSIGNED REFERENCES files (id),
+    category_id        INT UNSIGNED NOT NULL REFERENCES categories (id),
+    city_id            INT UNSIGNED NOT NULL REFERENCES cities (id),
+    is_executor        BOOLEAN               DEFAULT FALSE,
+    is_busy            BOOLEAN               DEFAULT FALSE,
+    is_hidden_contacts BOOLEAN               DEFAULT FALSE,
+    created_at         DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at         DATETIME     NOT NULL,
+    deleted_at         DATETIME     NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE tasks
+(
     PRIMARY KEY (id),
-    id                      INT                NOT NULL             auto_increment,
-    email                   VARCHAR(128)       NOT NULL             UNIQUE,
-    name                    VARCHAR(255)       NOT NULL,
-    password                VARCHAR(255)       NOT NULL,
-    phone                   VARCHAR(255)       NOT NULL,
-    telegram                VARCHAR(255)       NOT NULL,
-    description             VARCHAR(255)       NOT NULL,
-    birth_date              DATETIME                                DEFAULT NULL,
-    avatar                  VARCHAR(255)                            DEFAULT NULL,
-    token                   VARCHAR(255)                            DEFAULT NULL,
-    categorie_id            INT                NOT NULL             REFERENCES  categories(id),
-    role_id                 INT                NOT NULL             REFERENCES  roles(id),
-    city_id                 INT                NOT NULL             REFERENCES  cities(id),
-    is_executor             BOOLEAN                                 DEFAULT FALSE,
-    is_busy                 BOOLEAN                                 DEFAULT FALSE,
-    is_hidden_contacts      BOOLEAN                                 DEFAULT FALSE,
-    created_at              DATETIME           NOT NULL             DEFAULT NOW(),
-    updated_at              DATETIME           NOT NULL,
-    deleted_at              DATETIME           NOT NULL
+    id          INT UNSIGNED NOT NULL auto_increment,
+    title       VARCHAR(255) NOT NULL,
+    budget      INT          NULL,
+    status      TINYINT      NOT NULL DEFAULT 1,
+    customer_id INT UNSIGNED NOT NULL REFERENCES users (id),
+    executor_id INT UNSIGNED NULL REFERENCES users (id),
+    category_id INT UNSIGNED NOT NULL REFERENCES categories (id),
+    city_id     INT UNSIGNED REFERENCES cities (id),
+    coordinates POINT,
+    expiry_dt   DATETIME     NULL,
+    created_at  DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at  DATETIME     NOT NULL,
+    deleted_at  DATETIME     NOT NULL
 );
 
-CREATE TABLE tasks (
+CREATE TABLE tasks_files
+(
     PRIMARY KEY (id),
-    id                      INT                NOT NULL             auto_increment,
-    title                   VARCHAR(255)       NOT NULL,
-    budget                  INT                                     DEFAULT NULL,
-    author_id               INT                NOT NULL             REFERENCES  users(id),
-    executor_id             INT                                     REFERENCES  users(id),
-    categorie_id            INT                NOT NULL             REFERENCES  categories(id),
-    status_id               INT                NOT NULL             REFERENCES  statuses(id),
-    city_id                 INT                                     REFERENCES  cities(id),
-    latitude                VARCHAR(255)                            DEFAULT NULL,
-    longitude               VARCHAR(255)                            DEFAULT NULL,
-    expiry_dt               DATETIME                                DEFAULT NULL,
-    created_at              DATETIME           NOT NULL             DEFAULT NOW(),
-    updated_at              DATETIME           NOT NULL,
-    deleted_at              DATETIME           NOT NULL
+    id         INT UNSIGNED NOT NULL auto_increment,
+    task_id    INT UNSIGNED NOT NULL REFERENCES tasks (id),
+    file_id    INT UNSIGNED NOT NULL REFERENCES files (id),
+    created_at DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at DATETIME     NOT NULL,
+    deleted_at DATETIME     NOT NULL
 );
 
-CREATE TABLE responses (
+CREATE TABLE responses
+(
     PRIMARY KEY (id),
-    id                      INT                NOT NULL             auto_increment,
-    comment                 VARCHAR(255)       NOT NULL,
-    price                   INT                NOT NULL,
-    author_id               INT                NOT NULL             REFERENCES  users(id),
-    task_id                 INT                NOT NULL             REFERENCES  tasks(id),
-    is_rejected             BOOLEAN                                 DEFAULT FALSE,
-    created_at              DATETIME           NOT NULL             DEFAULT NOW(),
-    updated_at              DATETIME           NOT NULL,
-    deleted_at              DATETIME           NOT NULL
+    id          INT UNSIGNED NOT NULL auto_increment,
+    comment     VARCHAR(500) NOT NULL,
+    price       INT          NOT NULL,
+    executor_id INT UNSIGNED NOT NULL REFERENCES users (id),
+    task_id     INT UNSIGNED NOT NULL REFERENCES tasks (id),
+    is_rejected BOOLEAN               DEFAULT FALSE,
+    created_at  DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at  DATETIME     NOT NULL,
+    deleted_at  DATETIME     NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE reviews
+(
     PRIMARY KEY (id),
-    id                      INT                NOT NULL             auto_increment,
-    comment                 VARCHAR(255)       NOT NULL,
-    rating                  INT                NOT NULL,
-    author_id               INT                NOT NULL             REFERENCES  users(id),
-    executor_id             INT                NOT NULL             REFERENCES  users(id),
-    task_id                 INT                NOT NULL             REFERENCES  tasks(id),
-    created_at              DATETIME           NOT NULL             DEFAULT NOW(),
-    updated_at              DATETIME           NOT NULL,
-    deleted_at              DATETIME           NOT NULL
+    id          INT UNSIGNED NOT NULL auto_increment,
+    comment     VARCHAR(500) NOT NULL,
+    rating      TINYINT      NOT NULL,
+    customer_id INT UNSIGNED NOT NULL REFERENCES users (id),
+    executor_id INT UNSIGNED NOT NULL REFERENCES users (id),
+    task_id     INT UNSIGNED NOT NULL REFERENCES tasks (id),
+    created_at  DATETIME     NOT NULL DEFAULT NOW(),
+    updated_at  DATETIME     NOT NULL,
+    deleted_at  DATETIME     NOT NULL
 );
